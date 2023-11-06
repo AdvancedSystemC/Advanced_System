54945d18d494e16702dc79c47365880a66903288
benoit.duvernay <benoit.duvernay@orange.com>
2023-11-06 11:19:46 +0100

    Fin TP4

--- TP4/src/compteur.c
+++ TP4/src/compteur.c
@@ -1,35 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <unistd.h>
 
 // Compteur initialisé à 0
-int counter = 0;
+static int counter = 0;
 
 // Fonction de gestion du signal SIGINT
 void handle_sigint(int sig) {
     counter++;
+    printf(" Nombre de SIGINT détectées : %d\n" , counter);
 }
 
 // Fonction de gestion du signal SIGTERM
 void handle_sigterm(int sig) {
-    printf("Fin du programme\n");
+    printf("Signal SIGTERM reçu. Fin du programme.\n");
     exit(0);
 }
 
 int main() {
+
+    // Affiche le PID - plus simple pour kill
+    printf("PID du processus : %d\n", getpid());
+
     // Associer la fonction de gestion du signal SIGINT au signal
     signal(SIGINT, handle_sigint);
-
     // Associer la fonction de gestion du signal SIGTERM au signal
     signal(SIGTERM, handle_sigterm);
 
     // Boucle sans fin
     while (1) {
-        // Attendre les signaux
+        // rien
     }
-
-    // Afficher le nombre de signaux SIGINT reçus lorsque CTRL+C est pressé
-    printf("Nombre de signaux SIGINT reçus : %d\n", counter);
 
     return 0;
 }
