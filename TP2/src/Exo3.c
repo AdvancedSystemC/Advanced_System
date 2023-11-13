fe31e2b5c533dd73fcf143e475e888aeef757ae4
max1lock <alex.gerberon@gmail.com>
2023-11-13 14:55:11 +0100

    Exo3 fonctionnel

--- TP2/src/Exo3.c
+++ TP2/src/Exo3.c
@@ -1,59 +1,79 @@
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
-int main(){
-    int pipefd[2];
-    pid_t pid;
-
-    //Création du pipe
-    if(pipe(pipefd) == -1){
-        perror("Erreur lors de la création du pipe");
+int main() {
+    // Création d'un tube pour la communication entre ps et grep
+    int pipe_fd[2];
+    if (pipe(pipe_fd) == -1) {
+        perror("pipe");
         exit(EXIT_FAILURE);
     }
 
-    //Creation du processys fils
-    pid = fork();
+    // Création d'un processus fils
+    pid_t child_pid = fork();
 
-    if(pid < 0){
-        perror("Erreur lors de la création du processus fils");
+    if (child_pid == -1) {
+        perror("fork");
         exit(EXIT_FAILURE);
     }
-    else if(pid == 0){
-        //Processus Fils
 
-        // Fermeture de l'extrémité d'écriture du pipe
-        close(pipefd[0]);
+    if (child_pid == 0) { // Code du fils
+        // Fermeture du côté lecture du tube
+        close(pipe_fd[0]);
 
-        // Redirection de l'entrée standard vers l'extrémité de lecture du pipe
-        dup2(pipefd[1], STDIN_FILENO);
+        // Redirection de la sortie standard (stdout) vers le tube
+        dup2(pipe_fd[1], STDOUT_FILENO);
+        close(pipe_fd[1]);
 
-        // Exécution de la commande "ps eaux"
+        // Exécution de la commande "ps eaux" avec execlp
         execlp("ps", "ps", "eaux", NULL);
 
-        // En cas d'erreur lors de l'exécution de la commande
-        perror("Erreur lors de l'exécution de la commande ps");
+        // En cas d'erreur
+        perror("execlp");
         exit(EXIT_FAILURE);
-    }
-    else
-    {
-        // Processus parent
-        // Fermeture de l'extrémité d'écriture du pipe
-        close(pipefd[1]);
+    } else { // Code du parent
+        // Fermeture du côté écriture du tube
+        close(pipe_fd[1]);
 
-        // Redirection de l'entrée standard vers l'extrémité de lecture du pipe
-        dup2(pipefd[0], STDIN_FILENO);
+        // Redirection de l'entrée standard (stdin) depuis le tube
+        dup2(pipe_fd[0], STDIN_FILENO);
+        close(pipe_fd[0]);
 
-        // Exécution de la commande "grep "^root " > /dev/null"
-        execlp("grep", "grep", "^root", NULL);
+        // Création d'un nouveau processus pour exécuter le grep
+        pid_t grep_pid = fork();
 
-        // En cas d'erreur lors de l'exécution de la commande
-        perror("Erreur lors de l'exécution de la commande grep");
-        exit(EXIT_FAILURE);
+        if (grep_pid == -1) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+
+        if (grep_pid == 0) { // Code du fils (grep)
+            // Redirection de la sortie standard (stdout) vers /dev/null
+            int dev_null = open("/dev/null", O_WRONLY);
+            dup2(dev_null, STDOUT_FILENO);
+            close(dev_null);
+
+            // Exécution de la commande "grep "^root ""
+            execlp("grep", "grep", "^root", NULL);
+
+            // En cas d'erreur
+            perror("execlp");
+            exit(EXIT_FAILURE);
+        } else { // Code du parent
+            // Attente de la fin du fils (grep)
+            waitpid(grep_pid, NULL, 0);
+
+            // Affichage du message "root est connecté"
+            write(STDOUT_FILENO, "root est connecté\n", 18);
+        }
+
+        // Attente de la fin du fils (ps)
+        waitpid(child_pid, NULL, 0);
     }
 
     return 0;
-
 }
